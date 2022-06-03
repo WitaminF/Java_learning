d18f9601569f24a6e8c3615839320a5da6b1f816
WitaminF <scheiblerf@gmail.com>
2022-06-03 14:22:51 +0200

    Première app Java Hello World !

---
+++ FirstApp/src/Vue/Client.java
@@ -0,0 +1,16 @@
+package Vue;
+
+public class Client {
+
+    public Client(){
+        ok();
+    }
+    private void ok(){
+        System.out.println("Hello World !");
+    }
+
+    public static void main(String[] args) {
+        Client cl = new Client();
+
+    }
+}
